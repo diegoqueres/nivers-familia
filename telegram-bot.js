530c5bdd3c451b74f093b9ed1106414867a12bf0
Diego Queres <diegoqueres81@gmail.com>
2020-04-25 21:32:31 -0300

    Initial commit (with telegram bot script)

---
+++ telegram-bot.js
@@ -0,0 +1,29 @@
+const superagent = require('superagent'); 
+let botToken = null;
+
+const telegram_bot = {
+  initialize: (token) => {
+    botToken=token;
+    console.log('Bot initialized.');
+  },
+
+  sendMessage: async(chatId, message) => {
+    console.log(`Bot is sending message..`);
+    
+    await superagent
+      .post(`https://api.telegram.org/bot${botToken}/sendMessage`)
+      .send({ chat_id: `${chatId}`, text: message })
+      .set('Accept', 'application/json')
+      .then(res => {
+        if (res.body.ok)
+          console.log('Message has been set successfully!');
+      })
+      .catch(err => {
+        console.log(err.message);  
+      });
+
+  },
+
+}
+
+module.exports = telegram_bot;
