f59a0e243373fc4cc7a07c7ac8d62c4b9d8f2ce1
Diego Queres <diegoqueres81@gmail.com>
2020-04-27 09:19:57 -0300

    Express correction on index page

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
 app.get('/',function(req,res){
     res.send("nivers-familia-mota application is running =-D");
 });
+
+app.listen(8081);
