45bf581f3fad6d9001c449e5ef641c9df3e8f7c7
Diego Queres <diegoqueres81@gmail.com>
2020-04-27 09:11:22 -0300

    Test variables reseted

--- alert.js
+++ alert.js
@@ -1,7 +1,6 @@
 const bot = require('./telegram-bot');
-const BOT_TOKEN = '981005398:AAHBRZb420C5nmfQ8F5UZ4rMFWod2QSJT-0';
-const CANAL_MT_TASKS_ALERT_ID = -1001169830477; 
-const MINUTES_TO_CHECK = 3;
+const BOT_TOKEN = '';
+const CANAL_MT_TASKS_ALERT_ID = null; 
 
 (async () => {
     //Initializing alerts...
